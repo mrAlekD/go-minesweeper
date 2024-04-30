234309b2a9abb65d494db1ee9095c71951e55021
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-30 17:04:23 +0300

    parse flags

--- cmd/minesweeper/main.go
+++ cmd/minesweeper/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
 	"minesweeper/internal/tui"
 	"minesweeper/pkg/minesweeper"
 )
 
 func main() {
-	game, err := minesweeper.NewGame(minesweeper.Params{
-		Rows:  10,
-		Cols:  10,
-		Mines: 10,
-	})
+	game, err := minesweeper.NewGame(parseParams())
 	if err != nil {
 		panic(err)
 	}
 	if err := tui.Run(&game); err != nil {
 		panic(err)
 	}
+}
+
+func parseParams() minesweeper.Params {
+	params := minesweeper.Params{
+		Rows:  10,
+		Cols:  10,
+		Mines: 10,
+	}
+
+	flag.IntVar(&params.Rows, "rows", params.Rows, "number of rows")
+	flag.IntVar(&params.Cols, "cols", params.Cols, "number of columns")
+	flag.IntVar(&params.Mines, "mines", params.Mines, "number of mines")
+	flag.Parse()
+	return params
 }
