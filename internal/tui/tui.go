7c0d5377512fe56aeeb05559ea7ced4cc290a51d
mrAlekD <31899692+mrAlekD@users.noreply.github.com>
2024-04-30 16:58:39 +0300

    added marks

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -8,11 +8,13 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var defaultColor = lipgloss.Color("249")
+var defaultColor = lipgloss.Color("247")
+var markedColor = lipgloss.Color("47")
 var shownColor = lipgloss.Color("39")
 var emptyColor = lipgloss.Color("241")
 var mineColor = lipgloss.Color("160")
 var cellStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true).Padding(0, 1).Foreground(defaultColor).BorderForeground(defaultColor)
+var markedCellStyle = cellStyle.Copy().Foreground(markedColor).BorderForeground(markedColor)
 var shownCellStyle = cellStyle.Copy().Foreground(shownColor).BorderForeground(shownColor)
 var emptyCellStyle = cellStyle.Copy().Foreground(emptyColor).BorderForeground(emptyColor)
 var mineCellStyle = cellStyle.Copy().Foreground(mineColor).BorderForeground(mineColor)
@@ -20,7 +22,8 @@ var cellWidth = cellStyle.GetHorizontalFrameSize() + 1
 var cellHeight = cellStyle.GetVerticalFrameSize() + 1
 
 type Model struct {
-	game *minesweeper.Game
+	game   *minesweeper.Game
+	marked map[minesweeper.CellCoordinate]bool
 }
 
 func Run(game *minesweeper.Game) error {
@@ -30,7 +33,7 @@ func Run(game *minesweeper.Game) error {
 }
 
 func NewModel(game *minesweeper.Game) *Model {
-	return &Model{game}
+	return &Model{game, make(map[minesweeper.CellCoordinate]bool)}
 }
 
 func (m *Model) Init() tea.Cmd {
@@ -46,13 +49,20 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		case "r":
 			m.game.Init()
+			m.marked = make(map[minesweeper.CellCoordinate]bool)
 			return m, nil
 		}
 	case tea.MouseMsg:
-		if msg.Button == tea.MouseButtonLeft && msg.Action == tea.MouseActionRelease {
+		if msg.Action == tea.MouseActionRelease {
 			row := (msg.Y - 1) / cellHeight
 			col := msg.X / cellWidth
-			m.game.Press(minesweeper.CellCoordinate{Row: row, Col: col})
+			switch msg.Button {
+			case tea.MouseButtonLeft:
+				m.game.Press(minesweeper.CellCoordinate{Row: row, Col: col})
+			case tea.MouseButtonRight:
+				coord := minesweeper.CellCoordinate{Row: row, Col: col}
+				m.marked[coord] = !m.marked[coord]
+			}
 		}
 		return m, nil
 	}
@@ -65,18 +75,23 @@ func (m *Model) View() string {
 	rows := make([]string, 0, m.game.Params().Rows+1)
 	rows = append(rows, "State: "+states[m.game.State()])
 	cells := m.game.Cells()
-	for _, row := range cells {
+	for r, row := range cells {
 		items := make([]string, m.game.Params().Cols)
-		for i, cell := range row {
+		for c, cell := range row {
 			switch cell {
 			case minesweeper.Mine:
-				items[i] = mineCellStyle.Render("M")
+				items[c] = mineCellStyle.Render("M")
 			case minesweeper.Hidden:
-				items[i] = cellStyle.Render("X")
+				coord := minesweeper.CellCoordinate{Row: r, Col: c}
+				if m.marked[coord] {
+					items[c] = markedCellStyle.Render("X")
+					continue
+				}
+				items[c] = cellStyle.Render("?")
 			case 0:
-				items[i] = emptyCellStyle.Render(" ")
+				items[c] = emptyCellStyle.Render(" ")
 			default:
-				items[i] = shownCellStyle.Render(strconv.Itoa(int(cell)))
+				items[c] = shownCellStyle.Render(strconv.Itoa(int(cell)))
 			}
 		}
 		rows = append(rows, lipgloss.JoinHorizontal(lipgloss.Top, items...))
